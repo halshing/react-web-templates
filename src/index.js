7d0ab462f3aa5e1fe3fd6e726576f6b958e295ba
Hasan Shingieti <hasan.alshingieti@gmail.com>
2020-03-28 02:19:44 -0400

    app created

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// components
+import App from "./main/components/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/main/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// styles
+import "../styles/App.scss";
+
+const App = () => {
+  return <div>React Web Templates</div>;
+};
+
+export default App;
